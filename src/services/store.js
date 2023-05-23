b6800d1fa63c605782b3f298f04d48f3f94bb219
ROHAN BHATIA <rohanb459@gmail.com>
2023-05-23 18:52:34 +0530

    completed

--- src/services/store.js
+++ src/services/store.js
@@ -1,11 +1,10 @@
-import {configureStore} from '@reduxjs/toolkit'
-import { articleApi } from './article'
+import { configureStore } from "@reduxjs/toolkit";
 
-export const store = configureStore(
-    {
-        reducer: {
-            [articleApi.reducerPath] : articleApi.reducer
-        },
-        middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(articleApi.middleware)
-    }
-)
+import { articleApi } from "./article";
+
+export const store = configureStore({
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
